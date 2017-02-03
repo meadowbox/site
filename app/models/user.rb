70aae47cbb92403b8d9ba3c56e6c5a456e96deb2
Isaac Noda <isaacnoda@gmail.com>
2017-02-02 21:39:17 -0600

    Cleans user data before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
-  before_save :clean_data
+  before_validation :clean_data
 
   private
 
